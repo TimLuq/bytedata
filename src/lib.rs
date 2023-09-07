579248d423523ba7996b91dddf1af7bd9e1948ea
TimLuq <timluq+gitcommit@gmail.com>
2023-09-07 12:08:37 +0200

    added `const_slice` and `const_slice_str`

--- src/lib.rs
+++ src/lib.rs
@@ -84,5 +84,41 @@ pub const fn const_ends_with(haystack: &[u8], needle: &[u8]) -> bool {
     true
 }
 
+/// Helper function for slicing slices in a `const` context. Can be used to replace [`slice::get`](https://doc.rust-lang.org/std/primitive.slice.html#method.get) or brackets in `b[1..4]`.
+pub const fn const_slice(a: &'_ [u8], range: core::ops::Range<usize>) -> Option<&'_ [u8]> {
+    let start = range.start;
+    let end = range.end;
+    if start > end || end > a.len() {
+        return None;
+    }
+    unsafe { Some(core::slice::from_raw_parts(a.as_ptr().add(start), end - start)) }
+}
+
+/// An error that can occur when slicing a `str`.
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum StrSliceError {
+    /// The slice would cause the result to be out of bounds of the original `str`.
+    OutOfBounds,
+    /// The slice would cause the result to be split on a UTF-8 char boundary.
+    InvalidUtf8,
+}
+
+/// Helper function for slicing `str`s in a `const` context. Can be used to replace [`str::get`](https://doc.rust-lang.org/std/primitive.str.html#method.get) or brackets in `s[1..4]`.
+pub const fn const_slice_str(a: &'_ str, range: core::ops::Range<usize>) -> Result<&'_ str, StrSliceError> {
+    let a = a.as_bytes();
+    let start = range.start;
+    let end = range.end;
+    if start > end || end > a.len() {
+        return Err(StrSliceError::OutOfBounds);
+    }
+    if start != 0 && a[start] & 0b1100_0000 == 0b1000_0000 {
+        return Err(StrSliceError::InvalidUtf8);
+    }
+    if end != a.len() && a[end] & 0b1100_0000 == 0b1000_0000 {
+        return Err(StrSliceError::InvalidUtf8);
+    }
+    unsafe { Ok(core::str::from_utf8_unchecked(core::slice::from_raw_parts(a.as_ptr().add(start), end - start))) }
+}
+
 #[cfg(test)]
 mod test;
