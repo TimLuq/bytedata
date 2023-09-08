1c54d6f39b196db7e9cc36119f17571941cd018f
TimLuq <timluq+gitcommit@gmail.com>
2023-09-08 11:10:44 +0200

    added constant splitting

--- src/lib.rs
+++ src/lib.rs
@@ -177,6 +177,14 @@ pub const fn const_or_str<'a>(value: Option<&'a str>, default: &'a str) -> &'a s
     }
 }
 
+/// Simple helper function to return a constant byte sequence or a default sequence.
+pub const fn const_or_bytes<'a>(value: Option<&'a [u8]>, default: &'a [u8]) -> &'a [u8] {
+    match value {
+        Some(value) => value,
+        None => default,
+    }
+}
+
 /// Helper function to build a constant array of bytes from a list of byte slices and a known maximum buffer length.
 ///
 /// Will panic if the buffer is too small.
@@ -200,6 +208,63 @@ pub const fn build_const_bytes<const N: usize>(
         i += 1;
     }
     (data, p)
+}
+
+/// Helper function to split a constant slice of bytes on a specific byte.
+/// The matching byte will still be there in the second slice.
+pub const fn const_split_once_byte(haystack: &'_ [u8], needle: u8) -> Option<(&'_ [u8], &'_ [u8])> {
+    let mut p = 0;
+    while p < haystack.len() {
+        if haystack[p] == needle {
+            let a = const_or_bytes(const_slice(haystack, 0..p), &[]);
+            let b = const_or_bytes(const_slice(haystack, p..haystack.len()), &[]);
+            return Some((a, b));
+        }
+        p += 1;
+    }
+    None
+}
+
+/// Helper function to split a constant sequence of bytes on a specific sequence of bytes.
+/// The matching bytes will still be there in the second slice.
+pub const fn const_split_once_bytes<'a>(
+    haystack: &'a [u8],
+    needle: &'_ [u8],
+) -> Option<(&'a [u8], &'a [u8])> {
+    let mut p = 0;
+    if needle.len() > haystack.len() {
+        return None;
+    }
+    let max_start = haystack.len() - needle.len();
+    loop {
+        let hs = unsafe { core::slice::from_raw_parts(haystack.as_ptr().add(p), needle.len()) };
+        if !const_eq(hs, needle) {
+            p += 1;
+            if p <= max_start {
+                continue;
+            }
+            return None;
+        }
+        let a = unsafe { core::slice::from_raw_parts(haystack.as_ptr(), p) };
+        return Some((a, hs));
+    }
+}
+
+/// Helper function to split a constant str of bytes on a specific substring.
+/// The matching substring will still be there in the second string.
+pub const fn const_split_once_str<'a, const N: usize>(
+    haystack: &'a str,
+    needle: &'_ str,
+) -> Option<(&'a str, &'a str)> {
+    match const_split_once_bytes(haystack.as_bytes(), needle.as_bytes()) {
+        Some((a, b)) => unsafe {
+            Some((
+                core::str::from_utf8_unchecked(a),
+                core::str::from_utf8_unchecked(b),
+            ))
+        },
+        None => None,
+    }
 }
 
 #[cfg(test)]
