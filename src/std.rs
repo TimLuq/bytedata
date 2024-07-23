ed97c12c94fe8d20ef7903015e7ea1fabd16aa62
TimLuq <timluq+gitcommit@gmail.com>
2024-07-23 13:58:42 +0200

    optimized `<SharedBytesBuilder as io::Write>::write_vectored`

--- src/std.rs
+++ src/std.rs
@@ -160,4 +160,43 @@ impl Write for SharedBytesBuilder {
     fn flush(&mut self) -> std::io::Result<()> {
         Ok(())
     }
+
+    fn write_vectored(&mut self, bufs: &[std::io::IoSlice<'_>]) -> std::io::Result<usize> {
+        fn inner(targ: &mut [core::mem::MaybeUninit<u8>], bufs: &[std::io::IoSlice<'_>]) -> usize {
+            let max = targ.len();
+            let mut targ = targ.as_mut_ptr() as *mut u8;
+            let mut len = 0;
+            for buf in bufs {
+                let l = buf.len().min(max - len);
+                if l != 0 {
+                    unsafe {
+                        core::ptr::copy_nonoverlapping(buf.as_ptr(), targ, l);
+                        targ = targ.add(l);
+                    }
+                    len += l;
+                }
+            }
+            len
+        }
+        let mut len = 0;
+        for buf in bufs {
+            len += buf.len();
+        }
+        if len == 0 {
+            return Ok(0);
+        }
+        self.reserve(len);
+        let len = self.apply_unfilled(|targ| {
+            let len = inner(targ, bufs);
+            (len, len)
+        });
+        if len != 0 {
+            Ok(len)
+        } else {
+            return Err(std::io::Error::new(
+                std::io::ErrorKind::WriteZero,
+                "write_vectored failed to write any data as the buffer is full",
+            ));
+        }
+    }
 }
